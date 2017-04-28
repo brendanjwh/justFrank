af2d63b6faa0351d154950e9319c589d0c9b28d2
DZuz <danielzuzevich@gmail.com>
2017-04-28 08:38:14 -0400

    arrows work again, and user can cycle through all images

--- src/components/Slider/RightArrow.js
+++ src/components/Slider/RightArrow.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const RightArrow = (props) => {
   return (
-    <div className="nextArrow">
+    <div className="nextArrow" onClick={props.nextSlide}>
       <i className="fa fa-arrow-right fa-2x" aria-hidden="true"></i>
     </div>
   );
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -7,7 +7,10 @@ require('./style.scss');
 
 /* To Do */
 /*
-  => Add Dots & Arrow Funtionality
+  => Dot Functionality should be next
+  => User is now able to cycle through all images, back and forth.
+     However, I am concerned that using only one slide component won't work
+     with react transitions, but I am not sure yet.
 */
 
 export default class Slider extends Component {
@@ -17,7 +20,32 @@ export default class Slider extends Component {
     // User just needs to set the name of each picture in whatever order they would like.
     // This array is passed down to the child Slide as props.
     this.state = {
-      background: ['aurora','canyon','martin']
+      background: ['aurora','canyon','martin'],
+      current: 0
+    }
+
+    this.previousSlide = this.previousSlide.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
+  }
+
+  previousSlide() {
+    let current = this.state.current;
+    let imageArray = this.state.background.length - 1;
+
+    if(current >= 1)
+      this.setState({ current: current - 1 })
+    if(current === 0)
+      this.setState({ current: imageArray })
+  }
+
+  nextSlide() {
+    let current = this.state.current;
+    let imageArray = this.state.background.length - 1;
+
+    if((current >= 0) && (current !== imageArray))
+      this.setState({ current: current + 1 })
+    if(current === imageArray) {
+      this.setState({ current: 0 })
     }
   }
 
@@ -25,7 +53,11 @@ export default class Slider extends Component {
     return (
       <div className="slider">
         {/* There are no more individual slides, you only need one Slide Component */}
-        <Slide background={this.state.background} current={1} />
+        <Slide background={this.state.background} current={this.state.current} />
+
+        {/* Arrow Functionality */}
+        <LeftArrow previousSlide={this.previousSlide} />
+        <RightArrow nextSlide={this.nextSlide} />
       </div>
     );
   }
